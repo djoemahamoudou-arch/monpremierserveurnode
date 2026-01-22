1eb500137e11d3458df91ccac1ee0336e9690ff8
djoemahamoudou-arch <djoemahamoudou@gmail.com>
2026-01-22 16:33:28 +0300

    J'ai relié le serveur et l'application expresseJS

--- serveur.js
+++ serveur.js
@@ -5,12 +5,28 @@
 
 const http = require('http');
 
-// Maintenant , je crée mon serveur en utilisant le package HTTP
+//J'import l'application express app.JS
+const app = require('./app')
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(app);
+
+//Je configure le numero du port utilisé dans le serveur 
+app.set('port', 3000);
+
+
+
+
+const date = new Date();
+const heure = new Date();
+
+// Maintenant , je crée mon serveur en utilisant le package HTTP
+/*const server = http.createServer((req, res) => {
     res.end("Bonjour , je suis le serveur !");
-});
+});*/
 
 // Je précise qu'elle adresse et le port du serveur 
-server.listen(3000);
+server.listen(3000, ()=> {
+    console.log("le serveur tourne sur le port 3000");
+    console.log(date.toLocaleDateString(), heure.toLocaleTimeString());
+});
 
