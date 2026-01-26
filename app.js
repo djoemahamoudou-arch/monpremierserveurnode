1771dd0ef86c54821e19c407400811f9af04242b
djoemahamoudou-arch <djoemahamoudou@gmail.com>
2026-01-26 10:58:57 +0300

    J'ai intégrer les middleware dans l'app.js

--- app.js
+++ app.js
@@ -10,11 +10,31 @@ const express =  require('express');
 // J'initialise une application 'express()'
 const app = express();
 
-app.use((req,res) =>{
-    res.end("bonjour, je suis le serveur ! rachid");
+
+//J'ai utiliser un Middleware 1 pour afficher dans mon git bash.
+
+app.use((req, res, next) =>{
+    console.log("je suis un middleware dans le serveur ! rachid");
+    next();
+    
 
 });
+
+// J'ai utiliser "res.json()" pour que afficher que le "json dans ma page web ou dans postman", quand on fait localhost:3000.
+app.use((req, res,next) =>{
+    res.json({message:"salut vous être bien dans le serveur rachid"})
+
+    next();
+});
+
+
+// On a utiliser "res.end" c'est mon pour le dernier
+app.use((req, res) =>{
+    res.end("bonjour, je suis le serveur ! rachid");
+
     
+});   
+
 
 
 
