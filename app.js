85141d438992fbd23cfae4f79e1880632b256ecf
djoemahamoudou-arch <djoemahamoudou@gmail.com>
2026-01-27 11:21:27 +0300

    on a console.log l'objet pour l'afficher sur gitbash

--- app.js
+++ app.js
@@ -20,22 +20,50 @@ app.use((req, res, next) =>{
 
 });
 
-// J'ai utiliser "res.json()" pour que afficher que le "json dans ma page web ou dans postman", quand on fait localhost:3000.
-app.use((req, res,next) =>{
-    res.json({message:"salut vous être bien dans le serveur rachid"})
+// j
 
+/*J'ai utiliser "res.json()" pour que afficher que le "json dans ma page web ou dans postman", quand on fait localhost:3000.
+//app.use((req, res,next) =>{
+    res.json({message:"salut vous être bien dans le serveur rachid"})
     next();
-});
+});*/
 
 
 // On a utiliser "res.end" c'est mon pour le dernier
-app.use((req, res) =>{
-    res.end("bonjour, je suis le serveur ! rachid");
+app.use((req, res,next) =>{
+    console.log("bonjour, je suis le serveur ! rachid");
+    next();
 
     
 });   
 
+app.use('/api/fruit',(req,res,next) => {
+    console.log("Je passe dans la route /api/fruit");
+    next();
+    
 
+});
+
+app.use('/api/fruit',(req,res) => {
+    console.log("Je passe dans la route /api/fruit");
+    // mon fruit
+    const fruit = [
+        {
+            id:1,
+            nom:"pomme",
+            descrption: "fruit saisonner rieche en vitamine c",
+            prix:3
+        }
+
+
+
+
+
+        
+    ];
+    res.json(fruit);
+
+});
 
 
 // J'export l'application express sous forme de module
